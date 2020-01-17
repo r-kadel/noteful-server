7ccdfc8c12c69815b4301f2d32ccdd2aa510f8df
Ryan Kadel <ryan.kadel88@gmail.com>
2020-01-17 11:13:56 -0500

    prep for deploy

--- seeds/seed.noteful_notes.sql
+++ seeds/seed.noteful_notes.sql
@@ -2,6 +2,6 @@ INSERT INTO noteful_notes (title, content, folder_id)
 VALUES
   ('Note 1', 'lorem garbage', 1),
   ('Note 2', 'more stuff here', 4),
-  ('note 3', 'lowercase note', 8),
+  ('note 3', 'lowercase note', 5),
   ('note double Folder', ' 2 notes 1 folder', 1),
   ('note 5', 'another in 4', 4);
